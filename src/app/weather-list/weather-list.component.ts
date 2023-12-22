f1bc563c56e3a390ae6db6d7d9d61c72fcc2d4ff
Alex Gutierrez Blancarte <egutierrezb@gmai.com>
2023-12-22 00:19:29 -0600

    Modifications for new api weather

--- src/app/weather-list/weather-list.component.ts
+++ src/app/weather-list/weather-list.component.ts
@@ -8,17 +8,17 @@ import { WeatherService } from '../service/weather.service';
 })
 export class WeatherListComponent implements OnInit {
   weatherList: Object;
-  climate: any;
+  currents: any;
   locationCity: any;
   constructor(private weatherService: WeatherService) { }
 
-  ngOnInit(){
+  ngOnInit() {
     this.weatherService.findAll().subscribe(
       data => {
         //weathelist is an object of objects
         this.weatherList = data;
         //Retrieving consolidated_weather from JSON Object
-        this.climate = this.weatherList["current"];
+        this.currents = this.weatherList["current"];
         //Retrieving location (latitude/longitude)
         this.locationCity = this.weatherList["location"];
         //Debugging purposes
@@ -29,15 +29,14 @@ export class WeatherListComponent implements OnInit {
 
   //Routine to check if we have a temperature
   //value with decimal value less or greater than .5 
-  checkTemperature(temp: string){
+  checkTemperature(temp: string) {
     //If temperatures decimal value is greater than .5,
     //then temperature value will be rounded to the next
     //upper value, and this value is going to be
     //greater than original
 
     //Rounded value greater than original?  
-    if(parseFloat(temp)<Math.round(parseFloat(temp)))
-    {
+    if (parseFloat(temp) < Math.round(parseFloat(temp))) {
       return true;
     }
     return false;
@@ -45,20 +44,20 @@ export class WeatherListComponent implements OnInit {
 
   //If we have a returning value of TRUE in checkTemperature
   //we have to round it to an upper value
-  roundTemperature(temp: string){
-      return Math.round(parseFloat(temp));   
+  roundTemperature(temp: string) {
+    return Math.round(parseFloat(temp));
   }
 
   //If we have a returning value of TRUE in checkTemperature
   //we have to round it to an lower value
-  floorTemperature(temp: string){
-    return Math.floor(parseFloat(temp));   
+  floorTemperature(temp: string) {
+    return Math.floor(parseFloat(temp));
   }
 
   //Method that formats the date
   //input: date in numeric format [YY/MM/DAY] 
   //output: date with YY and DAY in numeric format and MM in string format
-  formatDate(date: string){
+  formatDate(date: string) {
     let splittedString = date.split("-");
     let aux = splittedString[0];
     splittedString[0] = splittedString[2];
@@ -66,14 +65,13 @@ export class WeatherListComponent implements OnInit {
 
     let MapDates = new Map();
     let month;
-    let monthsNames = ["Months:","January","February","March","April","May","June","July","August","September","October","November","December"];
-    for(var i=1; i<=12; i++)
-    {
-      if(i<10)
-          month = "0".concat(i.toString());
+    let monthsNames = ["Months:", "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    for (var i = 1; i <= 12; i++) {
+      if (i < 10)
+        month = "0".concat(i.toString());
       else
-          month = i;
-      MapDates.set(month,monthsNames[i]);
+        month = i;
+      MapDates.set(month, monthsNames[i]);
     }
     let monthFullName = MapDates.get(splittedString[1]);
     let newDate = splittedString[0].concat("/").concat(monthFullName).concat("/").concat(splittedString[2]);
